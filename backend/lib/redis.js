b13f4542cb4a4f10f14fa65f723e7e0880025387
Nurlan <iamsamurai1991@gmail.com>
2024-12-09 15:38:43 +0100

    modified:   backend/.env 	modified:   backend/lib/redis.js 	modified:   backend/node_modules/.package-lock.json 	modified:   backend/node_modules/agent-base/node_modules/debug/package.json 	modified:   backend/node_modules/agent-base/node_modules/debug/src/browser.js 	modified:   backend/node_modules/agent-base/node_modules/debug/src/common.js 	modified:   backend/node_modules/call-bind/CHANGELOG.md 	modified:   backend/node_modules/call-bind/index.js 	modified:   backend/node_modules/call-bind/package.json 	modified:   backend/node_modules/call-bind/test/index.js 	modified:   backend/node_modules/es-define-property/CHANGELOG.md 	modified:   backend/node_modules/es-define-property/index.js 	modified:   backend/node_modules/es-define-property/package.json 	modified:   backend/node_modules/es-define-property/test/index.js 	modified:   backend/node_modules/es-define-property/tsconfig.json 	modified:   backend/node_modules/express/History.md 	modified:   backend/node_modules/express/package.json 	deleted:    backend/node_modules/fsevents/LICENSE 	deleted:    backend/node_modules/fsevents/README.md 	deleted:    backend/node_modules/fsevents/fsevents.d.ts 	deleted:    backend/node_modules/fsevents/fsevents.js 	deleted:    backend/node_modules/fsevents/fsevents.node 	deleted:    backend/node_modules/fsevents/package.json 	modified:   backend/node_modules/get-intrinsic/CHANGELOG.md 	modified:   backend/node_modules/get-intrinsic/index.js 	modified:   backend/node_modules/get-intrinsic/package.json 	modified:   backend/node_modules/get-intrinsic/test/GetIntrinsic.js 	deleted:    backend/node_modules/has-proto/.eslintrc 	deleted:    backend/node_modules/has-proto/.github/FUNDING.yml 	deleted:    backend/node_modules/has-proto/CHANGELOG.md 	deleted:    backend/node_modules/has-proto/LICENSE 	deleted:    backend/node_modules/has-proto/README.md 	deleted:    backend/node_modules/has-proto/accessor.d.ts 	deleted:    backend/node_modules/has-proto/accessor.js 	deleted:    backend/node_modules/has-proto/helpers/getDunder.js 	deleted:    backend/node_modules/has-proto/helpers/setDunder.js 	deleted:    backend/node_modules/has-proto/index.d.ts 	deleted:    backend/node_modules/has-proto/index.js 	deleted:    backend/node_modules/has-proto/mutator.d.ts 	deleted:    backend/node_modules/has-proto/mutator.js 	deleted:    backend/node_modules/has-proto/package.json 	deleted:    backend/node_modules/has-proto/test/accessor.js 	deleted:    backend/node_modules/has-proto/test/index.js 	deleted:    backend/node_modules/has-proto/test/mutator.js 	deleted:    backend/node_modules/has-proto/tsconfig.json 	modified:   backend/node_modules/https-proxy-agent/node_modules/debug/package.json 	modified:   backend/node_modules/https-proxy-agent/node_modules/debug/src/browser.js 	modified:   backend/node_modules/https-proxy-agent/node_modules/debug/src/common.js 	modified:   backend/node_modules/ioredis/node_modules/debug/package.json 	modified:   backend/node_modules/ioredis/node_modules/debug/src/browser.js 	modified:   backend/node_modules/ioredis/node_modules/debug/src/common.js 	modified:   backend/node_modules/mongoose/dist/browser.umd.js 	modified:   backend/node_modules/mongoose/lib/helpers/query/getEmbeddedDiscriminatorPath.js 	modified:   backend/node_modules/mongoose/lib/plugins/saveSubdocs.js 	modified:   backend/node_modules/mongoose/package.json 	modified:   backend/node_modules/mquery/node_modules/debug/package.json 	modified:   backend/node_modules/mquery/node_modules/debug/src/browser.js 	modified:   backend/node_modules/mquery/node_modules/debug/src/common.js 	modified:   backend/node_modules/nodemon/node_modules/debug/package.json 	modified:   backend/node_modules/nodemon/node_modules/debug/src/browser.js 	modified:   backend/node_modules/nodemon/node_modules/debug/src/common.js 	modified:   backend/node_modules/path-to-regexp/index.js 	modified:   backend/node_modules/path-to-regexp/package.json 	modified:   backend/package-lock.json

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,3 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+
+async function testConnection() {
+  try {
+    await redis.set("testKey", "Hello, Upstash!");
+    const value = await redis.get("testKey");
+    console.log("Value from Redis:", value);
+    redis.disconnect();
+  } catch (error) {
+    console.error("Error connecting to Redis:", error);
+  }
+}
+
+testConnection();
